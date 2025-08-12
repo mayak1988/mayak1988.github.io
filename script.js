1e4fa926f68ab8e3bcb2a21d8c648c033a0f9e45
Maya Kovalevsky <73478004+mayak1988@users.noreply.github.com>
2025-08-12 10:49:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ function ToggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+window.addEventListener('load', () => {
+  const navHeight = document.getElementById('desktop-nav').offsetHeight;
+  if (window.location.hash) {
+    const targetElement = document.querySelector(window.location.hash);
+    if (targetElement) {
+      // Wait a tiny bit to let browser do default jump
+      setTimeout(() => {
+        const elementTop = targetElement.getBoundingClientRect().top + window.pageYOffset;
+        window.scrollTo({
+          top: elementTop - navHeight,
+          behavior: 'smooth'
+        });
+      }, 10);
+    }
+  }
+});
