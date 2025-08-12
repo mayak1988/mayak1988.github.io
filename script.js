9249ee878ef56f69aee8d305c62fff4602249efa
Maya Kovalevsky <73478004+mayak1988@users.noreply.github.com>
2025-08-12 10:50:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,19 +12,4 @@ function ToggleMenu() {
     icon.classList.toggle("open");
 }
 
-window.addEventListener('load', () => {
-  const navHeight = document.getElementById('desktop-nav').offsetHeight;
-  if (window.location.hash) {
-    const targetElement = document.querySelector(window.location.hash);
-    if (targetElement) {
-      // Wait a tiny bit to let browser do default jump
-      setTimeout(() => {
-        const elementTop = targetElement.getBoundingClientRect().top + window.pageYOffset;
-        window.scrollTo({
-          top: elementTop - navHeight,
-          behavior: 'smooth'
-        });
-      }, 10);
-    }
-  }
-});
+
